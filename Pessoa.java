b516953ea944771e6698100e9a1900c9506fbd8c
guigaon <58703086+guigaon@users.noreply.github.com>
2021-12-09 11:05:12 -0300

    exercicio modulo 6

---
+++ Pessoa.java
@@ -0,0 +1,28 @@
+public class Pessoa {
+
+    int idade;
+    int cpf;
+    string nome;
+    string sobrenome;
+
+    /**
+     *
+     * Passe de parametro apenas a data sem as aspas
+     *
+     * */
+    public obterIdade(int anoNascimento, int anoAtual){
+        int this.idade = anoNascimento - anoAtual
+        return idade;
+    }
+
+    public void obterNome(string nome){
+        string this.nome = nome;
+    }
+
+    public void obterCpf(){
+        int this.cpf = cpf;
+
+
+    }
+
+}
